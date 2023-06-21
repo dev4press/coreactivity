f0f2ee6eaf3255fc9315f863f41444ec879e980e
Milan Petrovic <milan@gdragon.info>
2023-06-21 15:43:07 +0200

    Work in progress.

--- forms/content-dashboard.php
+++ forms/content-dashboard.php
@@ -2,11 +2,6 @@
     <div class="d4p-cards-wrapper">
 		<?php
 
-        \Dev4Press\Plugin\CoreActivity\Basic\DB::instance()->prepare("SELECT * FROM wp_posts", 1);
-		wp_dashboard_plugins_output('');
-
-        include_once (ABSPATH.'/wp-includes/theme-compat/sidebar.php');
-
         include( COREACTIVITY_PATH . 'forms/content-dashboard-overall.php' );
 		include( COREACTIVITY_PATH . 'forms/content-dashboard-statistics.php' );
 
