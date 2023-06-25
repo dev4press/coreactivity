d8d9272d7d4d7251952a07fb42b16b197b095dd0
Milan Petrovic <milan@gdragon.info>
2023-06-25 19:46:19 +0200

    Work in progress.

--- core/admin/panel/Logs.php
+++ core/admin/panel/Logs.php
@@ -2,10 +2,10 @@
 
 namespace Dev4Press\Plugin\CoreActivity\Admin\Panel;
 
-use Dev4Press\Plugin\CoreActivity\Log\Core;
+
 use Dev4Press\Plugin\CoreActivity\Table\Logs as LogsTable;
 use Dev4Press\Plugin\CoreActivity\Admin\Panel;
-use Dev4Press\v43\Service\GEOIP\GEOJSIO;
+
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -29,22 +29,6 @@ class Logs extends Panel {
 	}
 
 	public function header_fill() : string {
-		GEOJSIO::instance()->bulk( array( Core::instance()->get( 'server_ip' ), Core::instance()->get( 'ip' ) ) );
-
-		$server_ip  = GEOJSIO::instance()->locate( Core::instance()->get( 'server_ip' ) );
-		$visitor_ip = GEOJSIO::instance()->locate( Core::instance()->get( 'ip' ) );
-
-		$render = '<li class="d4p-nav-button coreactivity-header-ip">';
-		$render .= '<i class="d4p-icon d4p-ui-database" title="' . esc_attr__( "Server IP", "coreactivity" ) . '"></i>';
-		$render .= '<span>' . Core::instance()->get( 'server_ip' ) . '</span>';
-		$render .= $server_ip->flag();
-		$render .= '</li>';
-		$render .= '<li class="d4p-nav-button coreactivity-header-ip">';
-		$render .= '<i class="d4p-icon d4p-ui-user-square" title="' . esc_attr__( "Current Request IP", "coreactivity" ) . '"></i>';
-		$render .= '<span>' . Core::instance()->get( 'ip' ) . '</span>';
-		$render .= $visitor_ip->flag();
-		$render .= '</li>';
-
-		return $render;
+		return coreactivity_panel_header_ip_block();
 	}
 }
--- core/functions.php
+++ core/functions.php
@@ -1,5 +1,8 @@
 <?php
 
+use Dev4Press\Plugin\CoreActivity\Log\Core;
+use Dev4Press\v43\Service\GEOIP\GEOJSIO;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -12,4 +15,24 @@ function coreactivity_get_term_ids_from_taxonomy_term_ids( array $tt_ids ) : arr
 	) );
 
 	return $query->terms;
+}
+
+function coreactivity_panel_header_ip_block() : string {
+	GEOJSIO::instance()->bulk( array( Core::instance()->get( 'server_ip' ), Core::instance()->get( 'ip' ) ) );
+
+	$server_ip  = GEOJSIO::instance()->locate( Core::instance()->get( 'server_ip' ) );
+	$visitor_ip = GEOJSIO::instance()->locate( Core::instance()->get( 'ip' ) );
+
+	$render = '<li class="d4p-nav-button d4p-header-special-button">';
+	$render .= '<i class="d4p-icon d4p-ui-database" title="' . esc_attr__( "Server IP", "coreactivity" ) . '"></i>';
+	$render .= '<span>' . Core::instance()->get( 'server_ip' ) . '</span>';
+	$render .= $server_ip->flag();
+	$render .= '</li>';
+	$render .= '<li class="d4p-nav-button d4p-header-special-button">';
+	$render .= '<i class="d4p-icon d4p-ui-user-square" title="' . esc_attr__( "Current Request IP", "coreactivity" ) . '"></i>';
+	$render .= '<span>' . Core::instance()->get( 'ip' ) . '</span>';
+	$render .= $visitor_ip->flag();
+	$render .= '</li>';
+
+	return $render;
 }
