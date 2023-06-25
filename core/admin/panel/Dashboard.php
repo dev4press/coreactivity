c10f3ced9277bc054a908a2aa925e845832bff0c
Milan Petrovic <milan@gdragon.info>
2023-06-25 19:26:06 +0200

    Work in progress.

--- core/admin/panel/Dashboard.php
+++ core/admin/panel/Dashboard.php
@@ -16,7 +16,7 @@ class Dashboard extends PanelDashboard {
 			$this->sidebar_links[ 'plugin' ][ 'events' ] = array(
 				'icon'  => $this->a()->menu_items[ 'events' ][ 'icon' ],
 				'class' => 'button-primary',
-				'url'   => $this->a()->panel_url( 'events', '', '', $this->a()->is_menu_item_network_only( 'events' ) ),
+				'url'   => $this->a()->panel_url( 'events', '', '', $this->a()->get_menu_item_network_url_flag( 'events' ) ),
 				'label' => __( "Events", "coreactivity" ),
 				'scope' => $this->a()->menu_items[ 'events' ][ 'scope' ] ?? array()
 			);
@@ -25,7 +25,7 @@ class Dashboard extends PanelDashboard {
 		$this->sidebar_links[ 'plugin' ][ 'logs' ] = array(
 			'icon'  => $this->a()->menu_items[ 'logs' ][ 'icon' ],
 			'class' => 'button-primary',
-			'url'   => $this->a()->panel_url( 'logs', '', '', $this->a()->is_menu_item_network_only( 'logs' ) ),
+			'url'   => $this->a()->panel_url( 'logs', '', '', $this->a()->get_menu_item_network_url_flag( 'logs' ) ),
 			'label' => __( "Logs", "coreactivity" ),
 			'scope' => $this->a()->menu_items[ 'logs' ][ 'scope' ] ?? array()
 		);
